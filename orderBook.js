5d136a8a7ac42501273e70314da38f983765bb45
m-madelyn <m.madelyn@live.com>
2019-11-21 22:29:44 -0500

    implements requirements in read me and passes all tests

--- orderBook.js
+++ orderBook.js
@@ -1,149 +1,61 @@
-//WORKING FOR TESTS 1,  2 & 3////////////////////////////////////////////////////////////////////////////////////////
-// function reconcileOrder (existingBook, incomingOrder) {
-//   let updatedBook = []
-//   let existingBookOrderType = existingBook.map(function (order) { return order.type })
-//   let existingBookOrderQuantity = existingBook.map(function (order) { return order.quantity })
-//   let existingBookOrderPrice = existingBook.map(function (order) { return order.price })
-
-//   if (existingBook.length == 0){
-//     updatedBook = existingBook.concat(incomingOrder)
-//   } else if (incomingOrder.type == existingBookOrderType){ //doesn't need "existingBook.length > 0 &&"
-//     updatedBook = existingBook.concat(incomingOrder)
-//   } else if (incomingOrder.type !== existingBookOrderType &&
-//     incomingOrder.quantity !== existingBookOrderQuantity &&
-//     incomingOrder.price !== existingBookOrderPrice){
-//     updatedBook = existingBook.concat(incomingOrder)
-//   }
-  
-//   return updatedBook
-// }
-//const updatedBook = reconcileOrder(existingBook, incomingOrder)
-
-
-
-//WORKING FOR TESTS 1, 2 & 3\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
-
-
-//                               *Pick up from test 4*
-
-
-
-
-//                 Test 1 adds an order to the book when the book is empty and thus cannot fulfill the order
-// const existingBook = []
-// const incomingOrder = {  type: 'sell', quantity: 10, price: 6150  }
-
-// function reconcileOrder (existingBook, incomingOrder) {
-//   let updatedBook = []
-    
-//   if (existingBook.length == 0){
-//     updatedBook = existingBook.concat(incomingOrder)
-//   }     
-//   return updatedBook
-// }
-// const updatedBook = reconcileOrder(existingBook, incomingOrder)
-
-//                                               \Test 1\passing//
-
-
-//               Test 2 adds an order to the book when the book has orders of the corresponding type (i.e. a sell with no buys
-
-// const existingBook = [{ type: 'sell', quantity: 10, price: 6150 }]
-// const incomingOrder = { type: 'sell', quantity: 12, price: 6000 }
-
-// function reconcileOrder (existingBook, incomingOrder) {
-//   let updatedBook = []
-//   let existingBookOrderType = existingBook.map(function (order) { return order.type })
-    
-//   if (incomingOrder.type == existingBookOrderType){
-//     updatedBook = existingBook.concat(incomingOrder)
-//   } 
-    
-//   return updatedBook
-// }
-// const updatedBook = reconcileOrder(existingBook, incomingOrder)
-
-//                                                 \Test 2\passing//
-
-//                Test 3 adds an order to the book when the book has a corresponding order type but it does not match
-
-const existingBook = [{ type: 'buy', quantity: 10, price: 6150 }, {
-                        type: 'buy', quantity: 11, price: 1111 }]    //how do we loop through this array to check ALL existing orders?
-
-const incomingOrder = { type: 'sell', quantity: 12, price: 6000 }
-
-
-function reconcileOrder (existingBook, incomingOrder) {
+function reconcileOrder(existingBook, incomingOrder) {
   let updatedBook = []
-  let existingBookOrderType = existingBook.map(function (order) { return order.type })
-  let existingBookOrderQuantity = existingBook.map(function (order) { return order.quantity })
-  let existingBookOrderPrice = existingBook.map(function (order) { return order.price })
-
-  if (incomingOrder.type !== existingBookOrderType &&
-    incomingOrder.quantity !== existingBookOrderQuantity &&
-    incomingOrder.price !== existingBookOrderPrice){
+  let unemptyBook = existingBook.length > 0
+  let exOrderType = existingBook.map(function (order) {
+    return order.type
+  })
+  let exOrderQuantity = existingBook.map(function (order) {
+    return order.quantity
+  })
+  let exOrderPrice = existingBook.map(function (order) {
+    return order.price
+  })
+  let orderTypeMatch = incomingOrder.type == exOrderType
+  let orderQuantitiesMatch = incomingOrder.quantity == exOrderQuantity
+  let orderPricesMatch = incomingOrder.price == exOrderPrice
+  if (!unemptyBook ||
+    //(2)adds an order to the book when the book has orders of the corresponding type (i.e. a sell with no buys
+    unemptyBook && orderTypeMatch ||
+    //(3)adds an order to the book when the book has a corresponding order type but it does not match
+    unemptyBook && !orderTypeMatch &&
+    !orderQuantitiesMatch &&
+    !orderPricesMatch) {
     updatedBook = existingBook.concat(incomingOrder)
+    console.log({updatedBook})
   }
-    
+  //(4)fulfills an order and removes the matching order when the book contains a matching order of the same quantity
+  if (unemptyBook &&
+    !orderTypeMatch &&
+    orderQuantitiesMatch) {
+    updatedBook = existingBook.splice(1, 1)
+    console.log({updatedBook})
+  }
+  // line 49 Test 5 fulfills an order and reduces the matching order when the book contains a matching order of a larger quantity:
+  if (unemptyBook &&
+    !orderTypeMatch &&
+    incomingOrder.quantity < exOrderQuantity &&
+    orderPricesMatch) {
+    for (var i = 0; i < existingBook.length; i++) {
+      existingBook[i].quantity = existingBook[i].quantity - incomingOrder.quantity
+      i = existingBook.length
+    }
+    updatedBook = existingBook
+    console.log({updatedBook})
+  }
+  //Test 6 'partially fulfills an order, removes the matching order and adds the remainder of the order to the book when the book contains a matching order of a smaller quantity'
+  if (unemptyBook &&
+    !orderTypeMatch &&
+    incomingOrder.quantity > exOrderQuantity &&
+    orderPricesMatch) {
+    for (i = 0; i < existingBook.length; i++) {
+      incomingOrder.quantity = incomingOrder.quantity - existingBook[i].quantity
+      existingBook.splice([i], 1)
+      existingBook.push(incomingOrder)
+      return existingBook
+    }
+    updatedBook = existingBook
+    console.log({updatedBook})
+  }
   return updatedBook
 }
-const updatedBook = reconcileOrder(existingBook, incomingOrder)
-
-//                                                \Test 3\passing//
-
-
-
-//          Test 4 fulfills an order and removes the matching order when the book contains a matching order of the same quantity
-
-// const existingBook = [{ type: 'buy', quantity: 10, price: 6150 }]
-// const incomingOrder = { type: 'sell', quantity: 10, price: 6150 }
-
-// function reconcileOrder (existingBook, incomingOrder) {
-//   let updatedBook = []
-//   let existingBookOrderType = existingBook.map(function (order) { return order.type })
-//   let existingBookOrderQuantity = existingBook.map(function (order) { return order.quantity })
-//   let existingBookOrderPrice = existingBook.map(function (order) { return order.price })
-
-//   if (existingBook.length > 0 
-//     && incomingOrder.type !== existingBookOrderType 
-//     && incomingOrder.quantity == existingBookOrderQuantity 
-//     && incomingOrder.price == existingBookOrderPrice){
-//     updatedBook = existingBook.filter                                       //how do I delete everything in the existingBook?
-//   }
-  
-//   return updatedBook
-// }
-
-
-const updatedBook = reconcileOrder(existingBook, incomingOrder)
-
-let existingBookOrderType = existingBook.map(function (order) { return order.type })
-let existingBookOrderQuantity = existingBook.map(function (order) { return order.quantity })
-let existingBookOrderPrice = existingBook.map(function (order) { return order.price })
-console.log('existing book values')
-console.log(existingBookOrderType)
-console.log(existingBookOrderQuantity)
-console.log(existingBookOrderPrice)
-
-console.log('evaluates order values')
-console.log(incomingOrder.type !== existingBookOrderType)
-console.log(incomingOrder.quantity == existingBookOrderQuantity)
-console.log(incomingOrder.price == existingBookOrderPrice)
-
-
-console.log('incoming order values')
-console.log(incomingOrder.type) 
-console.log(incomingOrder.quantity)
-console.log(incomingOrder.price) 
-
-//test 4\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
-
-
-
-//                               Pick up from test 4
-
-
-console.log('Updated Book')
-//console.log(updatedBook)
-
 module.exports = reconcileOrder
